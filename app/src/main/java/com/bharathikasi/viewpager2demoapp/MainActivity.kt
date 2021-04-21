32a57c570754f5d84c494f01220453a73e47d767
bharathikasi <bharathi.kasi@anywhere.co>
2021-04-21 19:10:24 +0530

    viewpager2 with tablayout

--- app/src/main/java/com/bharathikasi/viewpager2demoapp/MainActivity.kt
+++ app/src/main/java/com/bharathikasi/viewpager2demoapp/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import androidx.viewpager.widget.ViewPager
 import androidx.viewpager2.widget.ViewPager2
 import com.bharathikasi.viewpager2demoapp.adapter.ViewPagerAdapter
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,5 +23,24 @@ class MainActivity : AppCompatActivity() {
         val lAdapter = ViewPagerAdapter(lImageList)
         val lViewPager = findViewById<ViewPager2>(R.id.ViewPager)
         lViewPager.adapter = lAdapter
+
+        val lTablayout = findViewById<TabLayout>(R.id.TabLayout)
+        TabLayoutMediator(lTablayout,lViewPager,object :TabLayoutMediator.TabConfigurationStrategy{
+            override fun onConfigureTab(tab: TabLayout.Tab, position: Int) {
+                tab.text = "Tab ${position}"
+            }
+        }).attach()
+
+        lTablayout.addOnTabSelectedListener(object :TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+        })
     }
 }
